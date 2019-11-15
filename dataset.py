6c40678bac399c14c7468b696a90adf7223414ff
rich.ren@mail.utoronto.ca <rich.ren@mail.utoronto.ca>
2019-11-14 22:08:45 -0500

    dataset: created a custom dataset class

---
+++ dataset.py
@@ -0,0 +1,25 @@
+import torch.utils.data as data
+
+
+class TurnDataset(data.Dataset):
+
+    def __init__(self, X, y):
+        ######
+
+        # 4.1 YOUR CODE HERE
+        # Note that X is the training data set and y is the labeled data set.
+        self.X = X
+        self.y = y
+        ######
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        ######
+
+        # 4.1 YOUR CODE HERE
+        features = self.X[index]
+        label = self.y[index]
+        return features, label
+        ######
